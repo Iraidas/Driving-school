875b0885d2f9b06e1307f3205f8866862797c9c2
iraida <ira.serq.1996@gmail.com>
2023-02-08 19:35:59 +0300

    исправлен слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,7 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 36acd9e868a0e8dab9979f6e462130ebfabefcc6
 const swiper = new Swiper('.swiper', {
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-<<<<<<< HEAD
-=======
-=======
-import Swiper from 'swiper';
-import 'swiper/css';
-
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
->>>>>>> acf7b389284522303cf1317ea4f985349c54a187
->>>>>>> 36acd9e868a0e8dab9979f6e462130ebfabefcc6
-});
+  });
